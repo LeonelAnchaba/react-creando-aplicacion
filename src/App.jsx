a03f29e0874855b015cce7addab1fa6abb683063
LeonelAnchaba <leonelanchaba@gmail.com>
2024-03-26 00:59:47 -0300

    Trabajo practico clase 50. Creando componentes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import SideBar from  "./components/SideBar"
+import './assets/css/app.css'
+import ContentWrapper from "./components/ContentWrapper"
+
+function App() {
+
+  return (
+    <>
+  <div id="wrapper">
+    <SideBar></SideBar>
+    <ContentWrapper></ContentWrapper>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
